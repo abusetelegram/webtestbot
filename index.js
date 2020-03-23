aad3b3f86b61f2c63dd3bc900bc0673dbf460cd8
NeverBehave <gayhub@never.pet>
2020-03-22 23:10:42 -0400

    init

---
+++ index.js
@@ -0,0 +1,92 @@
+const WebPageTest = require('webpagetest');
+const wpt = new WebPageTest('www.webpagetest.org',process.env.WEBPAGE_TOKEN);
+const got = require('got')
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+function poll(fn, timeout, interval) {
+    let endTime = Number(new Date()) + (timeout || 180000);
+    interval = interval || 10000;
+
+    var checkCondition = function(resolve, reject) { 
+        var ajax = fn();
+        // dive into the ajax promise
+        ajax.then( function(response){
+            // If the condition is met, we're done!
+            if(response.data.data.statusCode == 200) {
+                resolve(response.data.data);
+            }
+            // If the condition isn't met but the timeout hasn't elapsed, go again
+            else if (Number(new Date()) < endTime) {
+                setTimeout(checkCondition, interval, resolve, reject);
+            }
+            // Didn't match and too much time, reject!
+            else {
+                reject(new Error('timed out for ' + fn + ': ' + arguments));
+            }
+        });
+    };
+
+    return new Promise(checkCondition);
+}
+
+
+bot.start((ctx) => ctx.reply('直接输入文字获取Webpagetest结果'))
+bot.on('text', (ctx) => {
+    console.log(ctx.chat)
+    let url = null
+    if (ctx.message.entities) {
+        if (ctx.message.entities[0].type == 'url') {
+            url = ctx.message.text.substring(ctx,message.entities[0].offset, ctx.message.entities[0].length)
+        }
+    }
+
+    if (url) {
+        console.log(url)
+        wpt.runTest(url, {
+            logData: 0,
+            private: true,
+            firstViewOnly: true
+            },(err, data) => {
+                if (err) {
+                    console.log(err)
+                    ctx.reply("请求未完成：" + err.toString())
+                    return 
+                }
+
+                const { jsonUrl, testId, userUrl } = data
+
+                ctx.replyWithMarkdown(`成功生成[测试](${userUrl} )\`${testId}\`！截图将在报告完成后更新`)
+
+                poll(() => {
+                    return got(jsonUrl)
+                }).then(data => {
+                    let photoGroup = []
+                    for (let [key, value] of Object.entries(data.runs[0].images)) {
+                        photoGroup.push({
+                            type: 'photo',
+                            media: value,
+                            caption: `${testId}'s ${key}`
+                        })
+                    }
+                    ctx.replyWithMediaGroup(photoGroup)
+                }).catch(() => {
+                    ctx.reply("更新数据时发生错误，停止")
+                })
+            })
+    } else {
+        console.log("Failed: ", ctx.message)
+        ctx.reply("找不到合法的url")
+    }
+})
+
+
+exports.handler = (event, context, callback) => {
+    const tmp = JSON.parse(event.body); // get data passed to us
+    bot.handleUpdate(tmp); // make Telegraf process that data
+    return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+      statusCode: 200,
+      body: '',
+    });
+  };
